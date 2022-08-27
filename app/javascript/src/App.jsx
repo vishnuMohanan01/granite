43aed096a53d6018cc44b5e5f8e7d0a3b36d73be
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-08-27 12:13:29 +0530

    setup react environment

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { setAuthHeaders } from "./apis/axios";
+import { initializeLogger } from "./common/logger";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    /*eslint no-undef: "off"*/
+    initializeLogger();
+    setAuthHeaders(setLoading);
+    // logger.info("Never use console.log");
+    // logger.error("Never use console.error");
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ app/javascript/src/common/logger.js
@@ -0,0 +1,9 @@
+export const initializeLogger = () => {
+    /* eslint react-hooks/rules-of-hooks: "off" */
+    const Logger = require("js-logger");
+    Logger.useDefaults();
+    if (process.env.RAILS_ENV === "production") {
+      Logger.setLevel(Logger.OFF);
+    }
+  };
+  
