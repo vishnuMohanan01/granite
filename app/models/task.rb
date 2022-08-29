900aa7d7213175f8e0f8b62485ded2e261709e47
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-08-29 14:29:03 +0530

    added title non null constraint and title null validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-    
+    MAXIMUM_TITLE_LENGTH = 125
+    validates :title, {presence: true, length: {maximum: MAXIMUM_TITLE_LENGTH}}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_132316) do
+ActiveRecord::Schema.define(version: 2022_08_29_084949) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "title"
+    t.text "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
