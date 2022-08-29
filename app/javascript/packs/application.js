75f61c6e400a9d00daced288eb993db635ae30dd
vishnumohanan01 <vishnu.mohanan@bigbinary.com>
2022-08-29 15:37:11 +0530

    added reactJS to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "../stylesheets/application.scss"
+import "../stylesheets/application.scss";
 
-Rails.start()
-ActiveStorage.start()
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
+
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+ReactRailsUJS.useContext(componentRequireContext);
+
